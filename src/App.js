348ee9d05dafeaf48f329b61c2e2b197a23a6196
한상원 <wkas1921@naver.com>
2023-01-11 10:15:11 +0900

    state 활용연습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from './Say';
-import EventPractice from './EventPractice';
+// import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 import './App.css';
 
 function App() {
   // return <MyComponent name="React">React</MyComponent>;
-  return <EventPractice />;
+  return <ValidationSample />;
 }
 
 export default App;
--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Validation.css';
+import './ValidationSample.css';
 
 class ValidationSample extends React.Component {
   //상태 객체
@@ -9,9 +9,40 @@ class ValidationSample extends React.Component {
     validated: false,
   };
 
+  //입력값으로 password를 변경
   handleChange = (e) => {
     this.setState({
       password: e.target.value,
     });
   };
+
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === '0000', //비밀번호가 0000일 경우에 true
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange} //onChange이벤트가 발생하면 handleChange호출
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? 'success'
+                : 'failure'
+              : ''
+          } //클릭이 되었다면 this.state.validated 상태 판별, 아니면 빈값
+          //판별해서 validated가 true면 success, 아니면 failure 반환
+        />
+        <button onClick={this.handleButtonClick}>검증하기</button>
+      </div>
+    );
+  }
 }
+
+export default ValidationSample;
