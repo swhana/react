913fcedf51932fbff4ea5b819f2eb8e45b91ad39
swhana <swhan0428@gmail.com>
2023-01-16 22:35:14 +0900

    async, await 사용한 promise 함수 연습

--- src/increase.js
+++ src/increase.js
@@ -1,41 +1,46 @@
 function increase(number) {
   const promise = new Promise((resolve, reject) => {
-    //resolve는 성공, reject는 실패
     setTimeout(() => {
       const result = number + 10;
       if (result > 50) {
-        //50보다 높으면 에러 발생
         const e = new Error('NumberIsTooBig');
         return reject(e);
       }
-      resolve(result);
+      resolve(result); //number + 10후에 성공 처리
     }, 1000);
   });
   return promise;
 }
 
-increase(0)
-  .then((number) => {
-    //promise에서 resolve된 값은 .then을 통해 받아옴
-    console.log(number);
-    return increase(number);
-  })
-  .then((number) => {
-    console.log(number);
-    return increase(number);
-  })
-  .then((number) => {
-    console.log(number);
-    return increase(number);
-  })
-  .then((number) => {
-    console.log(number);
-    return increase(number);
-  })
-  .then((number) => {
-    console.log(number);
-    return increase(number);
-  })
-  .catch((e) => {
+async function runTasks() {
+  try {
+    let result = await increase(0);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+  } catch (e) {
     console.log(e);
-  });
+  }
+}
+
+runTasks();
